46a05f6d3ca7a202b48eaf9290a30bab2e2faf99
attudevalck <maximknockaert@gmail.com>
2025-01-30 13:46:31 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,44 @@
+basic.forever(function () {
+    if (input.isGesture(Gesture.LogoDown)) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+})
+basic.forever(function () {
+    if (input.isGesture(Gesture.LogoUp)) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+})
+basic.forever(function () {
+    if (input.isGesture(Gesture.TiltLeft)) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+})
+basic.forever(function () {
+    if (input.isGesture(Gesture.TiltRight)) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+})
